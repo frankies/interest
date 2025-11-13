810e17bda3bd7490d5376566f8870fe9fe1db97a
frankies <linzhw@gmail.com>
2025-11-13 05:45:52 +0000

    init

---
+++ src/main/java/com/play/springboot4_preview_playground/Springboot4PreviewPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.play.springboot4_preview_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4PreviewPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot4PreviewPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/play/springboot4_preview_playground/Springboot4PreviewPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.play.springboot4_preview_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4PreviewPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
